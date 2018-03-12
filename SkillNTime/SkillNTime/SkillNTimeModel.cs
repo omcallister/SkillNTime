52b3b0beb9075ebbcb5593a71d680f4cf182e4d2
Othelle <anthonyomcallister@gmail.com>
2018-03-11 23:36:52 -0400

    Layout

---
+++ SkillNTime/SkillNTime/SkillNTimeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillNTime
+{
+    public class SkillNTimeModel
+    {
+        public int Month { get; set; }
+        public int day { get; set; }
+        public IEnumerable<string> Names { get; set; }
+
+
+    }
+}
