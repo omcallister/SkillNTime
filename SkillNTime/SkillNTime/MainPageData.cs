5e7fb6776b4149bad8de897d1bf9a50e1fd77e6d
Othelle <anthonyomcallister@gmail.com>
2018-03-20 22:35:57 -0400

    dummy data

---
+++ SkillNTime/SkillNTime/MainPageData.cs
@@ -0,0 +1,27 @@
+﻿using SkillNTime.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillNTime
+{
+    public class MainPageData
+    {
+        public List<NavigationModel> Skills { get; set; }
+
+        public MainPageData()
+        {
+            Skills = new List<NavigationModel>();
+
+            for (int hours = 1; hours <= 4; hours++)
+            {
+                Skills.Add(new NavigationModel(
+                    hours, 4, new string[] { "running" }));
+                Skills.Add(new NavigationModel(
+                    hours, 9, new string[] { "Violin", "coding" }));
+            }
+        }
+    }
+}
---
+++ SkillNTime/SkillNTime/ViewModel/NavigationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillNTime.ViewModel
+{
+    public class NavigationModel
+    {
+        public int Hours { get; }
+        public int TimeSpent { get; }
+        public IEnumerable<string> SkillName { get; }
+
+        public NavigationModel(int hours, int timespent, IEnumerable<string> skillname)
+        {
+            Hours = hours;
+            TimeSpent = timespent;
+            SkillName = skillname;
+        }
+
+        public string SkillsAsString => string.Join(", ", SkillName);
+    }
+}
